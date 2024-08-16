69fc401452edcfc53b8615afcdcbe0ad2f0661ca
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-08-16 19:39:25 +0300

    upload latest changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,4 @@
 import classes from "./Counter.module.css";
-import { createStore } from "redux";
-
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  } else if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
 
 const Counter = () => {
   const toggleCounterHandler = () => {};
