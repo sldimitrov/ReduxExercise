ff35f32e2b869454bd9d426f591c405993084667
Slavi Dimitrov <slavidimitrov54@gmail.com>
2024-08-17 11:28:47 +0300

    Dispatch an action from within a Component and change the state derived from the store Redux feature

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import { Provider } from "react-redux";
 
 import "./index.css";
 import App from "./App";
+import store from "./store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
